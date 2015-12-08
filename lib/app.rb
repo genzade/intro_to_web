fd1bf6506558d1c0b67c4b2da96aa793a87653a4
Hamid Zade <hamidmamadzade@gmail.com>
2015-12-08 13:55:25 +0000

    challenge 14

--- lib/app.rb
+++ lib/app.rb
@@ -8,7 +8,25 @@ get '/secret' do
   "Secret page. Piss off!!"
 end
 
-get '/cat' do
-  @rand_name = ["Amigo", "Oscar", "Viking"].sample
+get '/random-cat' do
+  @name = ["Amigo", "Oscar", "Viking"].sample
   erb(:index)
 end
+
+post '/named-cat' do
+  p params
+  @name = params[:name]
+  erb(:index)
+end
+
+get '/cat-naming' do
+  erb(:cat_naming)
+end
+
+#  Write a new get route that renders only the cat-naming form
+#  Add a method attribute to your form element, with the value "post"
+#  Change your /named-cat route to use post instead of get
+#  Interact with your application
+#  Observe the URL query string when you submit the form
+#  Observe the printed params when you submit the form
+#  Explain your observations to your pair partner.
